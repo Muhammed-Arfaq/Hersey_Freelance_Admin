c3300799522bde8eac7a0e3a205877c787d6c86b
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-10 14:31:46 +0530

    login design updated

--- src/Admin/Authentication/Login/Login.jsx
+++ src/Admin/Authentication/Login/Login.jsx
@@ -135,7 +135,7 @@ export default function AdminLogin() {
         <div className="absolute -top-20 w-full h-full bg-no-repeat ">
           <img
             className="w-full"
-            style={{ height: "116.4%" }}
+            style={{ height: "128.1%" }}
             src={bg2}
             alt=""
           />
@@ -145,7 +145,7 @@ export default function AdminLogin() {
             <div className="w-full lg:w-4/12 px-4">
               <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg border-0 bg-gray-50 bg-opacity-50 backdrop-blur">
                 <div className="rounded-t mb-0 px-6 py-6">
-                  <div className="text-center mb-3">
+                  {/* <div className="text-center mb-3">
                     <h6 className="text-blueGray-700 text-sm font-bold">
                       Sign in with
                     </h6>
@@ -165,17 +165,17 @@ export default function AdminLogin() {
                       <img alt="..." className="w-5 mr-1" src={google} />
                       Google
                     </button>
-                  </div>
-                  <hr className="mt-6 border-b-1 border-blueGray-300" />
+                  </div> */}
+                  <hr className="mt-4 border-b-1 border-blueGray-300" />
                 </div>
                 <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                   <div className="text-blueGray-600 text-center mb-3 font-bold">
-                    <small>Or sign in with credentials</small>
+                    <h6 className="text-blueGray-700 text-sm font-bold">Sign in with credentials</h6>
                   </div>
                   <form onSubmit={eventHandler}>
                     <div className="relative w-full mb-3">
                       <label
-                        className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                        className="block uppercase mt-10 text-blueGray-600 text-xs font-bold mb-2"
                         htmlFor="grid-password"
                       >
                         Email
@@ -210,14 +210,14 @@ export default function AdminLogin() {
                     </div>
                     <div>
                       <label className="inline-flex items-center cursor-pointer">
-                        <input
+                        {/* <input
                           id="customCheckLogin"
                           type="checkbox"
                           className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
                         />
                         <span className="ml-2 text-sm font-semibold text-blueGray-600">
                           Remember me
-                        </span>
+                        </span> */}
                       </label>
                     </div>
 
