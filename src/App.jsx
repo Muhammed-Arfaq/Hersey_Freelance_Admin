55ff0576b689ff078ac267310ed7d05e9ac50d68
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-10 16:19:47 +0530

    404 page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminLogin from './Admin/Authentication/Login/Login';
 import CategoryManagement from './Admin/CategoryManagement/CategoryManagement';
 import AdminDashboard from './Admin/Dashboard/AdminDashboard';
+import PageNotFound from './Admin/PageNotFound/PageNotFound';
 import UserManagement from './Admin/UserManagement/UserManagement';
 import VendorManagement from './Admin/VendorManagement/VendorManagement';
 import ProtectedRoutes from './protectedRoutes';
@@ -17,6 +18,7 @@ function App() {
             <Route path='/admin/manageUser' element={<ProtectedRoutes><UserManagement /></ProtectedRoutes>} />
             <Route path='/admin/manageVendor' element={<ProtectedRoutes><VendorManagement /></ProtectedRoutes>} />
             <Route path='/admin/manageCategory' element={<ProtectedRoutes><CategoryManagement /></ProtectedRoutes>} />
+            <Route path='*' element={<PageNotFound/>}/> 
         </Routes>
       </Router>
     </div>
